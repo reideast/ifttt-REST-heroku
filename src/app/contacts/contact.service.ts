bd9a90fabb17376f702f403edf14dbb2761d6ae4
Andrew Reid East <andrew@andreweast.net>
2018-05-19 19:45:40 +0100

    Remove duplicate import to rxjs/../toPromise

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Contact} from './contact';
 import {Http, Response} from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+// import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ContactService {
