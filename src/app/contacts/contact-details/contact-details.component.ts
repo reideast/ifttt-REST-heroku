6063ac90ab65207522330ea595f65f9fbf41736c
Andrew Reid East <andrew@andreweast.net>
2018-05-19 18:18:20 +0100

    Define front-end Angular components

--- src/app/contacts/contact-details/contact-details.component.ts
+++ src/app/contacts/contact-details/contact-details.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Contact} from '../contact';
+import {ContactService} from "../contact.service";
 
 @Component({
-  selector: 'app-contact-details',
-  templateUrl: './contact-details.component.html',
-  styleUrls: ['./contact-details.component.css']
+    selector: 'contact-details',
+    templateUrl: './contact-details.component.html',
+    styleUrls: ['./contact-details.component.css']
 })
-export class ContactDetailsComponent implements OnInit {
+export class ContactDetailsComponent {
+    @Input()
+    contact: Contact;
 
-  constructor() { }
+    @Input()
+    createHandler: Function;
+    @Input()
+    updateHandler: Function;
+    @Input()
+    deleteHandler: Function;
 
-  ngOnInit() {
-  }
+    constructor(private contactService: ContactService) {}
 
+    createContact(contact: Contact) {
+        this.contactService.createContact(contact)
+            .then((newContact: Contact) => {
+                this.createHandler(newContact);
+            });
+    }
+
+    updateContact(contact: Contact): void {
+        this.contactService.updateContact(contact)
+            .then((updatedContact: Contact) => {
+                this.updateHandler(updatedContact);
+            });
+    }
+
+    deleteContact(contactId: String): void {
+        this.contactService.deleteContact(contactId)
+            .then((deletedContactId: String) => {
+                this.deleteHandler(deletedContactId);
+            });
+    }
 }
