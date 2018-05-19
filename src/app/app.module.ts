2b3eadc543e0eb3e8b0bd10064ff010a0a5f3de0
Andrew Reid East <andrew@andreweast.net>
2018-05-19 19:54:34 +0100

    Final dependencies linking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
@@ -12,7 +14,9 @@ import { ContactListComponent } from './contacts/contact-list/contact-list.compo
     ContactListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
