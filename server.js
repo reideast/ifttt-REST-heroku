3fe7e91db181e6e679fa8b7c5c5d47610c07e246
Andrew Reid East <andrew@andreweast.net>
2018-05-20 21:14:34 +0100

    Create 'and/AND' parsing function

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ function handleError(res, reason, message, code) {
  * Helper function to authenticate users against API_USERS
  * @param username String user identifier
  * @param key String That user's API key
- * @return String That user's IFTTT_KEY, or null if not valid
+ * @return String That user's IFTTT_KEY, or {null} if not valid
  */
 function authenticate(username, key) {
     var found = API_USERS.find(function (elem) {
@@ -92,20 +92,18 @@ app.post("/api/ifttt/shopping/and", function (req, res) {
                 handleError(res, "Invalid input: Missing key", "Must provide a user's 'key'", 400);
             } else {
                 var userIftttKey = authenticate(req.body.username, req.body.key);
-                console.log("Authentication finished. Found: " + userIftttKey);
+                console.log("Authentication finished. Found user's IFTTT API key: " + userIftttKey);
                 if (!userIftttKey) {
                     handleError(res, "Unknown username and key", "The provided username and key were not valid", 401);
                 } else {
-                    var shoppingItems = req.body.shoppingItems;
-
-                    // TODO: parse shoppingItems into an array of strings, each split on "AND"
-                    var items = [shoppingItems]; // TODO: dummy array
+                    // Parse shoppingItems into an array of strings, each split on "AND"
+                    var items = splitOnAnd(req.body.shoppingItems);
 
                     items.forEach(function (item) {
-                        // var itemJson = JSON.stringify({
+                        var tags = ""; // TODO: Optional Trello Tags
                         var itemJson = { // IFTTT JSON Format: Value1,2,3
                             value1: item, // Text of the Trello card
-                            value2: "Frozen Refrigerated Dairy", // Optional Trello Tags
+                            value2: "ParsedByApi, " + tags,
                             value3: ""
                         };
                         console.log("About to send: ", itemJson);
@@ -131,6 +129,31 @@ app.post("/api/ifttt/shopping/and", function (req, res) {
         }
     }
 });
+
+/**
+ * Split a string on each and/AND found
+ * @param str String Text that may contain the word and/AND
+ * @return Array An list of items split
+ */
+function splitOnAnd(str) {
+    var phrases = [];
+    if (str) { // Protects against null, undefined, and ""
+        var words = str.split(' ');
+        var startOfPhrase = 0;
+        words.forEach(function (word, index) {
+            if (word.toLowerCase() === 'and') {
+                if (index - startOfPhrase > 0) { // multiple ands in a row
+                    phrases.push(words.slice(startOfPhrase, index).join(" "));
+                }
+                startOfPhrase = index + 1;
+            }
+        });
+        if (startOfPhrase < words.length) {
+            phrases.push(words.slice(startOfPhrase).join(" "));
+        }
+    }
+    return phrases;
+}
 
 // CONTACTS API ROUTES BELOW
 
